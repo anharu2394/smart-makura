c06ca2d592f8f2102d556974bcc1ca053a42cc25
anharu2394 <haruan2394@gmail.com>
2018-01-25 16:11:32 +0900

    rootを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :sleep_data
   resources :users
+  root 'sleep_data#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
