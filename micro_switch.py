550c1d8ffab701c1df7c4651ba3cf27c653f5fef
anharu2394 <haruan2394@gmail.com>
2018-01-25 16:33:43 +0900

    Timezoneをかえた

--- micro_switch.py
+++ micro_switch.py
@@ -44,13 +44,13 @@ def measure_time(s_t,pin):
 def write_start():
     response = requests.get('http://localhost:3000/users/1.json').text
     json_data = json.loads(response)
-    sleep_datum = (None,str(json_data["set_hour"]) + ":" +str(json_data["set_min"]),datetime.now().strftime("%Y-%m-%d %H:%M:%S"),datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    sleep_datum = (None,str(json_data["set_hour"]) + ":" +str(json_data["set_min"]),datetime.now().astimezone(timezone('UTC')).strftime("%Y-%m-%d %H:%M:%S"),datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     sql = 'insert into sleep_data (finished_at,set_at,created_at,updated_at) values (?,?,?,?)'
     cur.execute(sql,sleep_datum)
     con.commit()
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return datetime.now().astimezone(timezone('UTC')).strftime("%Y-%m-%d %H:%M:%S")
 def write_finish(created_at):
-    sql = "UPDATE sleep_data SET finished_at = '{0}' WHERE created_at = '{1}' ;".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"),str(created_at))
+    sql = "UPDATE sleep_data SET finished_at = '{0}' WHERE created_at = '{1}' ;".format(datetime.now().astimezone(timezone('UTC')).strftime("%Y-%m-%d %H:%M:%S"),str(created_at))
     print(sql)
     cur.execute(sql)
     con.commit()
